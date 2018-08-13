e980266d1e09796781b09ad550df0452161d07e3
Bennett <bennettcheung@yahoo.com>
2018-08-13 14:58:47 -0700

    Refactored code to use NSArray to store names and UIViews

--- ScrollViewImageGalleries/ViewController.m
+++ ScrollViewImageGalleries/ViewController.m
@@ -11,13 +11,10 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (strong, nonatomic) UIImageView *imageView1;
-@property (strong, nonatomic) UIImageView *imageView2;
-@property (strong, nonatomic) UIImageView *imageView3;
+@property (strong, nonatomic) NSMutableArray <UIImageView *> *imageViews;
 
-@property (strong, nonatomic) NSString* imageName1;
-@property (strong, nonatomic) NSString* imageName2;
-@property (strong, nonatomic) NSString* imageName3;
+@property (strong, nonatomic) NSArray <NSString*> *imageNames;
+
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
 
 @end
@@ -25,30 +22,23 @@
 @implementation ViewController
 
 - (void)setupImageViews {
-    self.imageName1 = @"Lighthouse";
-    self.imageName2 = @"Lighthouse-night";
-    self.imageName3 = @"Lighthouse-in-Field";
+    self.imageNames = @[@"Lighthouse", @"Lighthouse-night", @"Lighthouse-in-Field"];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.imageView1 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:self.imageName1]];
-    [self.imageView1 setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-    [self.imageView1 setUserInteractionEnabled:YES];
+    self.imageViews = [[NSMutableArray alloc]init];
     
-    UITapGestureRecognizer *gesture1 = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(launchDetailImageView:)];
-    [self.imageView1 addGestureRecognizer:gesture1];
-    
-    self.imageView2 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:self.imageName2]];
-    [self.imageView2 setFrame:CGRectMake(self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height)];
-    [self.imageView2 setUserInteractionEnabled:YES];
-    
-    UITapGestureRecognizer *gesture2 = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(launchDetailImageView:)];
-    [self.imageView2 addGestureRecognizer:gesture2];
-    
-    self.imageView3 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:self.imageName3]];
-    [self.imageView3 setFrame:CGRectMake(self.view.frame.size.width * 2, 0, self.view.frame.size.width, self.view.frame.size.height)];
-    [self.imageView3 setUserInteractionEnabled:YES];
-    UITapGestureRecognizer *gesture3 = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(launchDetailImageView:)];
-    [self.imageView3 addGestureRecognizer:gesture3];
+    for (int i=0; i > [self.imageNames count]; i++)
+    {
+        UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:self.imageNames[i]]];
+        [imageView setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        [imageView setUserInteractionEnabled:YES];
+        
+        UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(launchDetailImageView:)];
+        [imageView addGestureRecognizer:gesture];
+        
+        [self.imageViews addObject:imageView];
+    }
+
 }
 
 - (void)viewDidLoad {
@@ -56,13 +46,14 @@
     
     [self setupImageViews];
  
-    CGFloat galleryWidth = self.imageView1.frame.size.width + self.imageView2.frame.size.width + self.imageView3.frame.size.width;
+    CGFloat galleryWidth = self.view.frame.size.width * [self.imageViews count];
     
     UIView *subView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, galleryWidth, self.view.frame.size.height)];
-//    [subView setUserInteractionEnabled:YES];
-    [subView addSubview:self.imageView1];
-    [subView addSubview:self.imageView2];
-    [subView addSubview:self.imageView3];
+
+    for (int i=0; i< [self.imageViews count]; i++)
+    {
+        [subView addSubview:self.imageViews[i]];
+    }
     
     
     [self.scrollView addSubview:subView];
@@ -89,12 +80,13 @@
         UIImageView* imageView = (UIImageView*)sender.view;
      
         NSString* imageName = @"";
-        if (imageView == self.imageView1)
-            imageName = self.imageName1;
-        else if (imageView == self.imageView2)
-            imageName = self.imageName2;
-        else if (imageView == self.imageView3)
-            imageName = self.imageName3;
+        
+        for (int i=0; i< [self.imageViews count]; i++)
+        {
+            if (self.imageViews[i] == imageView)
+            imageName = self.imageNames[i];
+        }
+        
         [self performSegueWithIdentifier:@"showImageDetail" sender:imageName];
     }
 }
